c92502089d98366c10fca6a67a392d4ac0270659
Luis Augusto <hello@luis.app>
2026-06-03 13:46:01 -0400

    Initial Commit

---
+++ VoiceGPTTests/VoiceGPTTests.swift
@@ -0,0 +1,19 @@
+//
+//  VoiceGPTTests.swift
+//  VoiceGPTTests
+//
+//  Created by Luis Augusto on 6/3/26.
+//
+
+import Testing
+@testable import VoiceGPT
+
+struct VoiceGPTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
